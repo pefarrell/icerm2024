500e9aecafd6e417adb2f3ac1a44d43874c1d87a
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2024-02-05 19:22:05 +0000

    ...

---
+++ 03_boundaries/01_robin.py
@@ -0,0 +1,25 @@
+from firedrake import *
+
+mesh = UnitSquareMesh(64, 64, quadrilateral=True)
+(x, y) = SpatialCoordinate(mesh)
+
+V = FunctionSpace(mesh, "CG", 1)
+
+u = Function(V, name="Solution")
+v = TestFunction(V)
+
+f = Constant(1)
+g = Constant(1)
+bc = DirichletBC(V, Constant(1), (1,))
+
+u.interpolate(Constant(5))
+F = (
+      inner(grad(u), grad(v))*dx
+    + inner(u, v)*ds
+    - inner(f, v)*dx
+    - inner(g, v)*ds
+    )
+
+solve(F == 0, u, bc)
+
+File("output/robin.pvd").write(u)
