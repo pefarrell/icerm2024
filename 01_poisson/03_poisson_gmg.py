eaba04e3015580ebd7bf27fe467c1fbb58f7e1a5
Patrick Farrell <patrick.farrell@maths.ox.ac.uk>
2024-09-22 10:58:38 +0100

    Simplify GMG example

--- 01_poisson/03_poisson_gmg.py
+++ 01_poisson/03_poisson_gmg.py
@@ -1,6 +1,6 @@
 from firedrake import *
 
-base = UnitSquareMesh(10, 10, quadrilateral=True)
+base = UnitSquareMesh(10, 10, diagonal="crossed")
 mh = MeshHierarchy(base, 3)
 mesh = mh[-1]
 
@@ -21,28 +21,17 @@ F = inner(grad(u), grad(v))*dx - f*v*dx
 
 
 sp = {
-    "mat_type": "matfree",
+    "mat_type": "aij",
     "ksp_type": "cg",
     "ksp_view": None,
     "ksp_monitor": None,
     "ksp_rtol": 1.0e-14,
     "pc_type": "mg",
-    "mg_coarse": {
-        "pc_type": "python",
-        "pc_python_type": "firedrake.AssembledPC",
-        "assembled_pc_type": "cholesky",
-    },
+    "mg_coarse_pc_type": "cholesky",
     "mg_levels": {
         "ksp_max_it": 1,
         "ksp_type": "chebyshev",
-        "pc_type": "python",
-        "pc_python_type": "firedrake.FDMPC",
-        "fdm": {
-            "pc_type": "python",
-            "pc_python_type": "firedrake.ASMExtrudedStarPC",
-            "pc_star_mat_ordering_type": "metisnd",
-            "pc_star_sub_sub_pc_type": "cholesky",
-        }
+        "pc_type": "jacobi",
     }
 }
 
